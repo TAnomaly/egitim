576e7e936db06a34afad16f9526388c9a0faf552
TAnomaly <tmirac@hotmail.com>
2025-11-16 03:58:04 +0300

    egitim

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,7 @@ use std::f64::consts::PI;
 extern "C" {
     #[wasm_bindgen(js_namespace = console)]
     fn log(s: &str);
-}
+// }
 
 macro_rules! console_log {
     ($($t:tt)*) => (log(&format_args!($($t)*).to_string()))
